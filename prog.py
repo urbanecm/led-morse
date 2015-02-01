3efe4a835eda84b8b15287eafaab60d948803c05
Martin Urbanec <martin@urbanec.cz>
2015-02-01 14:31:27 +0000

    Commit (changes unknown)

--- prog.py
+++ prog.py
@@ -14,25 +14,26 @@ try:
 		action = raw_input("Činnost: ")
 		
 		if (action == "1"):
-			print "1) Text -> morseovka"
-			print "2) Morseovka -> test"
-			print "3) Zpět"
-			action = raw_input("Činnost: ")
-			if (action == "1"):
-				text = raw_input("Zadejte text k převedení do morseovky: ")
-				codes = Module.textToCode(text)
-				for code in codes:
-					print code
-			elif (action == "2"):
-				print "Zadejte text psaný morseovkou (jeden znak, jeden řádek)"
-				print "Zadávání ukončete stiskem enter"
-				code = Module.readCode()
-				text = Module.codeToText(code)
-				print "Text převedený z morseovky: ", text
-			elif (action == "3)"):
-				continue
-			else:
-				print "Zvolena nesprávná akce"
+			while True:
+				print "1) Text -> morseovka"
+				print "2) Morseovka -> test"
+				print "3) Zpět"
+				action = raw_input("Činnost: ")
+				if (action == "1"):
+					text = raw_input("Zadejte text k převedení do morseovky: ")
+					codes = Module.textToCode(text)
+					for code in codes:
+						print code
+				elif (action == "2"):
+					print "Zadejte text psaný morseovkou (jeden znak, jeden řádek)"
+					print "Zadávání ukončete stiskem enter"
+					code = Module.readCode()
+					text = Module.codeToText(code)
+					print "Text převedený z morseovky: ", text
+				elif (action == "3"):
+					break
+				else:
+					print "Zvolena nesprávná akce"
 		elif (action == "2"):
 			print "1) Příjem"
 			print "2) Vysílání"
